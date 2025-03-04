25e9f18e48d3bf795d0f3a19e145fedaf6f4d41f
WenFra005 <wendellfrancisco2005@hotmail.com>
2025-03-04 12:16:21 -0300

    UPDATE and FIX: Desenvolve a função de extrato e corrige a função de sacar

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -4,7 +4,13 @@ def deposito(saldo, extrato):
     extrato.append(valor_deposito)
     return saldo
 
-def sacar(saldo):
+def sacar(saldo, extrato):
     valor_saque = input("Qual é o valor do saque?: ")
     saldo = saldo - valor_saque
+    extrato.append(valor_saque)
     return saldo
+
+def extrato(saldo, extrato):
+    extrato_list = extrato
+    print("Saldo: ", saldo)
+    print(extrato_list)
