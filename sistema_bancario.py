ff0cb0d23b868d8872f762bc204b933383ed26c7
WenFra005 <wendellfrancisco2005@hotmail.com>
2025-03-04 12:12:04 -0300

    UPDATE and FIX: Desenvolve a função de sacar e corrige a função de depósito.

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,4 +1,10 @@
-def deposito(saldo, valor_deposito):
+def deposito(saldo, extrato):
     valor_deposito = input("Qual é o valor do depósito?: ")
     saldo = saldo + valor_deposito
+    extrato.append(valor_deposito)
+    return saldo
+
+def sacar(saldo):
+    valor_saque = input("Qual é o valor do saque?: ")
+    saldo = saldo - valor_saque
     return saldo
