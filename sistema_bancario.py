00e719479837cb520b232d6cd56fdd420dafcfaa
WenFra005 <wendellfrancisco2005@hotmail.com>
2025-03-04 12:19:39 -0300

    UPDATE: desenvolve a função main

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -10,7 +10,29 @@ def sacar(saldo, extrato):
     extrato.append(valor_saque)
     return saldo
 
-def extrato(saldo, extrato):
+def exibir_extrato(saldo, extrato):
     extrato_list = extrato
     print("Saldo: ", saldo)
     print(extrato_list)
+
+def main():
+    saldo = 0
+    extrato = []
+    while True:
+        print("1 - Depositar")
+        print("2 - Sacar")
+        print("3 - Extrato")
+        print("4 - Sair")
+        opcao = input("Escolha uma opção: ")
+        if opcao == "1":
+            saldo = deposito(saldo, extrato)
+        elif opcao == "2":
+            saldo = sacar(saldo, extrato)
+        elif opcao == "3":
+            exibir_extrato(saldo, extrato)
+        elif opcao == "4":
+            break
+        else:
+            print("Opção inválida")
+
+main()
