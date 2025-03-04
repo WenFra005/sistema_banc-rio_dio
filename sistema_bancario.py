e24cde461ab663284c4f73fb9f174fa5e67265b2
WenFra005 <wendellfrancisco2005@hotmail.com>
2025-03-04 12:27:12 -0300

    FIX: corrige as funções depósito e sacar trocando os termos "input" e "int" de lugar

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,11 +1,11 @@
 def deposito(saldo, extrato):
-    valor_deposito = input("Qual é o valor do depósito?: ")
+    valor_deposito = int(input("Qual é o valor do depósito?: "))
     saldo = saldo + valor_deposito
     extrato.append(valor_deposito)
     return saldo
 
 def sacar(saldo, extrato):
-    valor_saque = input("Qual é o valor do saque?: ")
+    valor_saque = int(input("Qual é o valor do saque?: "))
     saldo = saldo - valor_saque
     extrato.append(valor_saque)
     return saldo
